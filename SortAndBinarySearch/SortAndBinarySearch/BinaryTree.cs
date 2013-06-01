153fcd907e9ea365ed18fb731dad6bf0bcbf8b5d
Evan Zhang <evanzhangxin@gmail.com>
2013-06-01 09:26:02 -0700

    Add Binary Tree defintiion and traversal function

---
+++ SortAndBinarySearch/SortAndBinarySearch/BinaryTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAndBinarySearch
+{
+    public class BinaryTree
+    {
+        public TreeNode Root { get; set; }
+    }
+
+    public class TreeNode
+    {
+        public int Data { get; set; }
+
+        public TreeNode LeftNode { get; set; }
+
+        public TreeNode RightNode { get; set; }
+
+        public void AddNode(int data)
+        {
+            if (data >= this.Data)
+            {
+                if (this.RightNode != null)
+                    this.RightNode.AddNode(data);
+                else
+                    this.RightNode = new TreeNode() { Data = data };
+
+            }
+            else
+            {
+                if (this.LeftNode != null)
+                    this.LeftNode.AddNode(data);
+                else
+                    this.LeftNode = new TreeNode() { Data = data };
+            }
+        }
+
+    }
+}
