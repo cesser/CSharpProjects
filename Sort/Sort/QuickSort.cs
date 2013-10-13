59651247193d74038c785ee359b0e141cff8d581
Evan Zhang <evanzhangxin@gmail.com>
2013-10-12 20:01:50 -0700

    implement quick sort

--- Sort/Sort/QuickSort.cs
+++ Sort/Sort/QuickSort.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Sort
 {
-    class QuickSort
+    public static class QuickSort
     {
+        public static void QuickSortTest()
+        {
+            MyArray array = new MyArray(10);
+            array.DisplayArray();
+
+            Console.WriteLine("Quick Sort");
+
+            QuickSort1(array);
+        }
+
+        private static void QuickSort1(MyArray array)
+        {
+            int key = 0;
+
+            for (int i = array.UpperIndex; i >= 0 ; i--)
+            {
+                if (array[key] > array[i])
+                {
+                    int temp = array[key];
+                    array[key] = array[i];
+                    array[i] = temp; 
+                }
+            }
+        }
     }
 }
