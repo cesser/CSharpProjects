3b9cb4a4e9f91a8a707d910edb75ba00900e01dd
Evan Zhang <evanzhangxin@gmail.com>
2013-10-29 14:59:39 -0700

    add project for interview programs

---
+++ InterviewPrograms/FindLongestCommonString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrograms
+{
+    public class FindLongestCommonString
+    {
+
+        public string FindLCS(string string1, string string2)
+        {
+            if(string.IsNullOrEmpty(string1) || string.IsNullOrEmpty(string2))
+                throw new NullReferenceException();
+
+            int[,] array = new int[string1.Length, string2.Length];
+            int longestNumber = -1;
+            string result = string.Empty;
+            for (int i = 0; i < string1.Length; i++)
+            {
+                for (int j = 0; j < string2.Length; j++)
+                {
+                    if (string1[i] == string2[j])
+                    {
+                        if (i > 0 && j > 0)
+                            array[i, j] = array[i - 1, j - 1] + 1;
+                        else if(i == 0 || j ==0)
+                            array[i, j] = 1;
+
+                        if (array[i, j] > longestNumber)
+                        {
+                            longestNumber = array[i, j];
+                            result = string1.Substring(i +1 -longestNumber, longestNumber);
+                        }
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ InterviewPrograms/UnitTestProject1/FindLongestCommonStringTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewPrograms;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FindLongestCommonStringTest
+    {
+        FindLongestCommonString findLCS = new FindLongestCommonString();
+        [TestMethod]
+        public void FindLCS1()
+        {
+            string string1 = "123abcdfabc";
+            string string2 = "jj12hhjgdsabcddk";
+            string result = findLCS.FindLCS(string1, string2);
+
+            Assert.IsTrue(string1.Contains(result));
+            Assert.IsTrue(string2.Contains(result));
+            Assert.AreEqual("abcd", result);
+        }
+
+        [TestMethod]
+        public void FindLCS2()
+        {
+            string string1 = "123";
+            string string2 = "as123df1234";
+            string result = findLCS.FindLCS(string1, string2);
+
+            Assert.IsTrue(string1.Contains(result));
+            Assert.IsTrue(string2.Contains(result));
+            Assert.AreEqual("123", result);
+        }
+    }
+}
