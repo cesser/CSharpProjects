7f9cd74f762edc13ce3cc179a8e82b27a61eb3dd
Evan Zhang <evanzhangxin@gmail.com>
2013-10-29 18:02:16 -0700

    add permutation

---
+++ InterviewPrograms/Permutation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrograms
+{
+    public class Permutation
+    {
+        public List<int[]> AlgorithmL(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<int>> RecusiveSlove(int[] array)
+        {
+            List<List<int>> result = new List<List<int>>();
+            List<int> selectionSet = array.ToList();
+            RecusivePermutation(new int[array.Length], 0, selectionSet, ref result);
+
+            return result;
+        }
+
+        private void RecusivePermutation(int[] result, int i, List<int> selectionSet, ref List<List<int>> results)
+        {
+            if (selectionSet.Count == 0)
+                CopyResultToList(result, results);
+              
+            //find a set doesn't have any duplicated elements
+            var nonDuplicationSet = RemoveDuplicatElement(selectionSet);
+
+            foreach (int s in nonDuplicationSet)
+            {
+                result[i++] = s;
+                RecusivePermutation(result, i, RemoveSpecificItem(selectionSet, s), ref results);
+            }
+
+        }
+
+        private void CopyResultToList(int[] array, List<List<int>> results)
+        {
+            if (array != null && results != null)
+            {
+                results.Add(array.ToList());
+            }
+        }
+
+        private List<int> RemoveSpecificItem(List<int> selectionSet, int s)
+        {
+            List<int> newList = new List<int>();
+            foreach (var item in selectionSet)
+            {
+                if (item != s)
+                    newList.Add(item);
+            }
+            return newList;
+        }
+
+        private List<int> RemoveDuplicatElement(List<int> selectionSet)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+            foreach (var item in selectionSet)
+            {
+                if (!hashSet.Contains(item))
+                    hashSet.Add(item);
+            }
+
+            return hashSet.ToList();
+        }
+    }
+}
---
+++ InterviewPrograms/UnitTestProject1/PermutationTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewPrograms;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for PermutationTest
+    /// </summary>
+    [TestClass]
+    public class PermutationTest
+    {
+        private Permutation permutation = new Permutation();
+
+        [TestMethod]
+        public void RecusivePermutation1()
+        {
+            int[] array = { 1,2,2,3};
+
+            List<List<int>> result = permutation.RecusiveSlove(array);
+            Assert.AreEqual(result.Count, 12);
+
+            foreach (var item in result)
+            {
+                PrintArray(item);
+            }
+
+            Console.Read();
+        }
+
+
+        private void PrintArray(List<int> array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
