f3f0206520f8d1c63d20a861046bace952c2eccc
Evan Zhang <evanzhangxin@gmail.com>
2013-11-10 22:14:51 -0800

    update graph and search project

---
+++ Search/Search/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = GenArray(20);
+            DisplayArray(array);
+            Search.SortArray_QuickSort(array);
+            DisplayArray(array);
+
+            while (true)
+            {
+                
+                
+                
+                
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Search.BinarySearch(number, array));
+
+            }
+            
+            Console.Read();
+        }
+
+        static int[] GenArray(int arrayLength)
+        {
+            int[] array = new int[arrayLength];
+
+            Random r = new Random();
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = r.Next(100);
+            }
+
+            return array;
+        }
+
+        static void DisplayArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Search/Search/Search.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    public static class Search
+    {
+
+        public static void SortArray_QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length -1);
+        }
+
+        private static void QuickSort(int[] array, int start, int end)
+        {
+            if (start >= end)
+                return;
+
+            int mid = Partition_QuickSort(array, start, end);
+            //DisplayArray(array, start, mid);
+            //DisplayArray(array, mid, end);
+            QuickSort(array, start, mid -1);
+            QuickSort(array, mid + 1, end);
+        }
+
+        private static void DisplayArray(int[] array, int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(array[i] + "  ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static int Partition_QuickSort(int[] array, int start, int end)
+        {
+            if (start >= end)
+                return start;
+
+            //use first element as mid
+            int midElement = array[end];
+
+            int tempIndex = start; //temp index is used to swap with smaller element
+            for (int i = start; i < end ; i++)
+            {
+                if (array[i] < midElement)
+                { 
+                    //swap temp index and i
+                    if (i != tempIndex)
+                    {
+                        int temp = array[i];
+                        array[i] = array[tempIndex];
+                        array[tempIndex] = temp; 
+                    }
+                    tempIndex++;
+                }
+            }
+
+            //move midelement to tempindex
+            array[end] = array[tempIndex];
+            array[tempIndex] = midElement;
+
+            return tempIndex;
+        }
+
+        public static int BinarySearch(int target, int[] array)
+        {
+            return BinarySearch(target, array, 0, array.Length -1);
+        }
+
+        private static int BinarySearch(int target, int[] array, int start, int end)
+        {
+            if (end < start)
+                return -1;
+
+            int midIndex = start + (end - start) / 2;
+
+            if (array[midIndex] == target)
+                return midIndex;
+
+            if (array[midIndex] < target)
+                return BinarySearch(target, array, midIndex + 1, end);
+            else
+                return BinarySearch(target, array, start, midIndex - 1);
+        }
+
+    }
+}
