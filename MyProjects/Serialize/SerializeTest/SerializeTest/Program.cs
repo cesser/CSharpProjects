c53dd1109ac07b40ae914cf3632ed3e4c8a3cec0
Evan Zhang <evanzhangxin@gmail.com>
2013-11-13 19:11:59 -0800

    move search and binary project to algorithm add serializer test project

---
+++ MyProjects/Serialize/SerializeTest/SerializeTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TestData;
+using TestData2;
+
+namespace SerializeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NewSerializer();
+
+            Stopwatch swatch = Stopwatch.StartNew();
+            XmlSerializer xs2 = new XmlSerializer(typeof(Class2));
+            Class2 c2 = new Class2() { Name = "Class2", ClassInfo = new Class5() { Name = "Class5" } };
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter sw = new StringWriter(sb) )
+            {
+                xs2.Serialize(sw, c2);
+            }
+
+            using (StringReader sr = new StringReader(sb.ToString()))
+            {
+                object obj = xs2.Deserialize(sr);
+            }
+            swatch.Stop();
+            Console.WriteLine(swatch.Elapsed.TotalSeconds);
+            Console.Read();
+        }
+
+        private static void NewSerializer()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            XmlSerializer xs = new XmlSerializer(typeof(Class1), new Type[] { typeof(Class2), typeof(Class3), typeof(Class4) });
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalSeconds);
+
+
+
+            sw.Restart();
+            XmlSerializer xs1 = new XmlSerializer(typeof(Class1));
+            XmlSerializer xs2 = new XmlSerializer(typeof(Class2));
+            XmlSerializer xs3 = new XmlSerializer(typeof(Class3));
+            XmlSerializer xs4 = new XmlSerializer(typeof(Class4));
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalSeconds);
+        }
+    }
+}
---
+++ MyProjects/Serialize/SerializeTest/TestData/Classes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TestData2;
+
+namespace TestData
+{
+    [Serializable]
+    public class Class1
+    {
+        public string Name { get; set; }
+    }
+
+    [Serializable]
+    [XmlInclude(typeof(Class5))]
+    public class Class2
+    {
+        public string Name { get; set; }
+
+        public object ClassInfo { get; set; }
+    }
+
+    [Serializable]
+    public class Class3
+    {
+        public string Name { get; set; }
+    }
+
+    [Serializable]
+    public class Class4
+    {
+        public string Name { get; set; }
+    }
+}
